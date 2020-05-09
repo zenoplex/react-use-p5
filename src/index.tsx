325bc1054991bac28a8c84e4bed2f763e13c19c0
Gen Ichino <zenoplex@gmail.com>
2020-05-10 01:43:36 +0900

    feat: implement useP5

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import * as React from 'react';
+import p5 from 'p5';
 
-interface Props {
-  text: string
-}
+export const useP5 = (
+  sketch: (sketch: p5) => void,
+): [(node: HTMLElement) => void] => {
+  const p5Ref = React.useRef<p5 | null>(null);
+  const canvasParentRef = React.useRef<HTMLElement | null>(null);
+  const setCanvasParentRef = React.useCallback((node: HTMLElement) => {
+    if (node) {
+      // eslint-disable-next-line new-cap
+      p5Ref.current = new p5(sketch, node);
+    } else {
+      // when component unmounts node is null
+      if (p5Ref.current) p5Ref.current.remove();
+    }
+    canvasParentRef.current = node;
+  }, []);
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+  return [setCanvasParentRef];
+};
