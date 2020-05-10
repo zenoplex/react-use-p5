a19a353b2d126f7da695fb2835169e4d414414fa
Gen Ichino <zenoplex@gmail.com>
2020-05-10 12:45:46 +0900

    refactor: update callback when sketch parameter change

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,19 @@ export const useP5 = (
 ): [(node: HTMLElement) => void] => {
   const p5Ref = React.useRef<p5 | null>(null);
   const canvasParentRef = React.useRef<HTMLElement | null>(null);
-  const setCanvasParentRef = React.useCallback((node: HTMLElement) => {
-    if (node) {
-      // eslint-disable-next-line new-cap
-      p5Ref.current = new p5(sketch, node);
-    } else {
-      // when component unmounts node is null
-      if (p5Ref.current) p5Ref.current.remove();
-    }
-    canvasParentRef.current = node;
-  }, []);
+  const setCanvasParentRef = React.useCallback(
+    (node: HTMLElement) => {
+      if (node) {
+        // eslint-disable-next-line new-cap
+        p5Ref.current = new p5(sketch, node);
+      } else {
+        // when component unmounts node is null
+        if (p5Ref.current) p5Ref.current.remove();
+      }
+      canvasParentRef.current = node;
+    },
+    [sketch],
+  );
 
   return [setCanvasParentRef];
 };
