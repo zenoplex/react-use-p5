05e48c8e6db3cf0c088e4560b501da417ee69b84
Gen Ichino <zenoplex@gmail.com>
2020-05-13 00:49:18 +0900

    test: add update sketch test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,19 +1,27 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
+import p5 from 'p5';
 import { useP5 } from '../index';
 
 describe('useP5', () => {
-  const sketch = (p): void => {
+  const sketch1 = (p: p5): void => {
     p.setup = (): void => {
       p.createCanvas(1000, 1000);
     };
   };
 
+  const sketch2 = (p: p5): void => {
+    p.setup = (): void => {
+      p.createCanvas(500, 500);
+    };
+  };
+
   interface Props {
+    sketch?: (p: p5) => void;
     unmount?: boolean;
   }
 
-  const Component = ({ unmount }: Props): JSX.Element => {
+  const Component = ({ sketch = sketch1, unmount }: Props): JSX.Element => {
     const [ref] = useP5(sketch);
     return <div data-testid='wrapper' ref={!unmount ? ref : undefined} />;
   };
@@ -25,7 +33,7 @@ describe('useP5', () => {
       setIsLoading(false);
     }, []);
 
-    const [ref] = useP5(sketch);
+    const [ref] = useP5(sketch1);
 
     return isLoading ? (
       <div>Loading</div>
@@ -56,5 +64,17 @@ describe('useP5', () => {
     // remove ref
     rerender(<Component unmount />);
     expect(wrapper.querySelector('canvas.p5Canvas')).toBeNull();
+  });
+
+  it('should update sketch', () => {
+    const { rerender, getByTestId } = render(<Component />);
+    const wrapper = getByTestId('wrapper');
+    expect(wrapper.querySelector('canvas.p5Canvas').getAttribute('style')).toBe(
+      'width: 1000px; height: 1000px;',
+    );
+    rerender(<Component sketch={sketch2} />);
+    expect(wrapper.querySelector('canvas.p5Canvas').getAttribute('style')).toBe(
+      'width: 500px; height: 500px;',
+    );
   });
 });
